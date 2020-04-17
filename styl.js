695b5c91ea09feb28503dab2df5ebc66aea38615
Klimek <klimkowskib@gmail.com>
2020-04-17 13:29:16 +0200

    1 cup ready in js

--- styl.js
+++ styl.js
@@ -1,10 +1,15 @@
 let kubki = document.querySelectorAll(".cup");
 
 function klik(evt){
-    console.log("klik");
+    let poziom = Number(this.getAttribute("poziom", "0"));
+    poziom += 16,5;
+    if (poziom < 99){
+        this.style.backgroundPosition = "0 " + poziom +"%";
+    }
+    this.setAttribute("poziom",poziom);
+    
 }
 
 kubki.forEach(function(kubek) {
     kubek.addEventListener('click', klik);
 })
-
