aff01d061726df4338fb56f19bc74094d57e64b3
Klimek <klimkowskib@gmail.com>
2020-04-17 13:57:42 +0200

    6 diff colors, add else cursor

--- styl.js
+++ styl.js
@@ -2,12 +2,14 @@ let kubki = document.querySelectorAll(".cup");
 
 function klik(evt){
     let poziom = Number(this.getAttribute("poziom", "0"));
-    poziom += 16,5;
+    poziom += 23,5;
     if (poziom < 99){
         this.style.backgroundPosition = "0 " + poziom +"%";
+        this.setAttribute("poziom",poziom);
     }
-    this.setAttribute("poziom",poziom);
-    
+    else {
+        this.style.cursor = "not-allowed"
+    }
 }
 
 kubki.forEach(function(kubek) {
